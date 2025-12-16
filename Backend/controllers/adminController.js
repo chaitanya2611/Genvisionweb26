3c0fed854215139f55dcd2868ccfba266fa4452a
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2025-12-16 20:59:54 +0530

    Update adminController.js

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -7,7 +7,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "genvision_secret";
 // 📍 Register Admin (IITB only)
 export const registerAdmin = async (req, res) => {
   try {
-    console.log("🔥 ADMIN REGISTER HIT 🔥");
+    console.log("REQ BODY 👉", req.body);
     const { email, password } = req.body;
 
     // 🔒 IITB email hard check

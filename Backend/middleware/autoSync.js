42998c556929a4f889b0009e084a5240b5d201a5
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2025-12-16 22:02:21 +0530

    Update autoSync.js

--- Backend/middleware/autoSync.js
+++ Backend/middleware/autoSync.js
@@ -1,12 +1,12 @@
 import cron from 'node-cron';
-import syncParticipants from './sheetSync.js';  // लक्षात ठेवा .js extension
+// import syncParticipants from './sheetSync.js';  // लक्षात ठेवा .js extension
 
 function startAutoSync() {
   // every 10 minutes
   cron.schedule('*/2 * * * *', async () => {
-    console.log('⏳ Auto Sync Started...');
-    await syncParticipants();
-    console.log('✅ Auto Sync Finished');
+    console.log('⏳ Pinged');
+    // await syncParticipants();
+    // console.log('✅ Auto Sync Finished');
   });
 }
 
